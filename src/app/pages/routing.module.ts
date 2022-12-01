0847e32592a55579df90d4dd3a11baa3d8356dfd
Hevelyn <hevelynsoares15@gmail.com>
2022-12-01 00:02:46 -0300

    first commit

---
+++ src/app/pages/routing.module.ts
@@ -0,0 +1,16 @@
+import { DetailsComponent } from './details/details.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+//   { path: '**', redirectTo: ''},
+  { path: 'details', component: DetailsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RoutingModule { }
